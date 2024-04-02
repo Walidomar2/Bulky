2b5164f22bf5dc3622ff20c68bdef92333d20d4f
Walid Omar <walidomar291@gmail.com>
2024-04-02 06:03:44 +0200

    applicationDbcontext class and creating the empty database

---
+++ BulkyWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿namespace BulkyWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        {
+            
+        }
+    }
+}
--- BulkyWeb/global.cs
+++ BulkyWeb/global.cs
@@ -1,3 +1,5 @@
 ﻿global using BulkyWeb.Controllers;
 global using BulkyWeb.Models;
 global using System.ComponentModel.DataAnnotations;
+global using Microsoft.EntityFrameworkCore;
+global using BulkyWeb.Data;
