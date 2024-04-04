75d87560965bbce90a4a2177d23dcfe0f3504531
Walid Omar <walidomar291@gmail.com>
2024-04-04 22:02:58 +0200

    deleting method and delete view

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -53,7 +53,42 @@
         [HttpPost]
         public IActionResult Edit(Category categoryModel)
         {
-            
+            if(!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            _context.Categories.Update(categoryModel);
+            _context.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Delete(int id)
+        {
+            if (id == 0)
+                return NotFound();
+
+            var category = _context.Categories.FirstOrDefault(c => c.Id == id);
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+
+        [HttpPost,ActionName("Delete")]
+        public IActionResult DeletePost(int id)
+        {   
+            var category = _context.Categories.FirstOrDefault(x => x.Id == id);
+
+            if (category == null)
+                return NotFound();
+
+            _context.Categories.Remove(category);
+            _context.SaveChanges();
             return RedirectToAction(nameof(Index));
         }
 
