14815925161f368f2f23eea86b5e888ffd5a94ad
Walid Omar <walidomar291@gmail.com>
2024-04-27 14:51:48 +0300

    product model and migrations

---
+++ Bulky.DataAccess/Repository/ProductRepository.cs
@@ -0,0 +1,30 @@
+﻿using Bulky.DataAccess.Data;
+using Bulky.DataAccess.Repository.IRepository;
+using Bulky.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bulky.DataAccess.Repository
+{
+    public class ProductRepository : Repository<Product>, IProductRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public ProductRepository(ApplicationDbContext context) : base(context)
+        {
+            _context = context;
+        }
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+
+        public void Update(Product product)
+        {
+            _context.Products.Update(product);
+        }
+    }
+}
