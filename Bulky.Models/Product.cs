051715075b708fbfa268cb61f83ef34b08b5fdc0
Walid Omar <walidomar291@gmail.com>
2024-04-27 16:30:56 +0300

    adding foreign key to product model

--- Bulky.Models/Product.cs
+++ Bulky.Models/Product.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -47,6 +48,11 @@ namespace Bulky.Models
         [Display(Name = "Price for 100+")]
         [Range(1, 1000)]
         public double Price100 { get; set; }
+
+        public int CategoryId { get; set; }
+
+        [ForeignKey("CategoryId")]
+        public Category Category { get; set; }
 
     }
 }
