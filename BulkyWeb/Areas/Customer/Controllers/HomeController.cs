c00f7522e2e23aa1962e32da3cfaeae58e17f35d
Walid Omar <walidomar291@gmail.com>
2024-05-21 19:16:03 +0300

    editing Home Page and adding products in it

--- BulkyWeb/Areas/Customer/Controllers/HomeController.cs
+++ BulkyWeb/Areas/Customer/Controllers/HomeController.cs
@@ -7,15 +7,18 @@ namespace BulkyWeb.Areas.Customer.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IProductRepository _productRepository;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IProductRepository productRepository)
         {
             _logger = logger;
+            _productRepository = productRepository;
         }
 
         public IActionResult Index()
         {
-            return View();
+            var products = _productRepository.GetAll(includeProperties:"Category");
+            return View(products);
         }
 
         public IActionResult Privacy()
